050dc5f721a36732b3784cad82d4759c9b5c8559
adi <adityachaudhary700@gmail.com>
2024-03-07 15:12:50 +0545

    advs list

--- ALM/settings.py
+++ ALM/settings.py
@@ -130,14 +130,14 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 
 # Default primary key field type
